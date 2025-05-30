04b94c26db43ab478fa90c6e1ea7aac1aab44451
Theylon Augusto <theylon.silva@hotmail.com>
2025-05-30 17:15:31 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   <title>Formulário de Consulta</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="bg-light">
+<body class="bg-dark">
 
   <div class="container mt-5">
     <h2 class="mb-4">Formulário de Consulta</h2>
